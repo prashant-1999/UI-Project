d2815ac6f191154113b11bef06f4bce8e5bdc581
Prashant Gupta <63060363+prashant-1999@users.noreply.github.com>
2022-08-01 17:23:20 +0530

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,81 @@
+var filter = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+
+// login
+const checkReg = ()=>{
+    
+    var name=document.getElementById('user').value;  
+    var password=document.getElementById('pass').value;  
+    
+    if (name==null || name=="" || password==""){  
+        alert("Name or password can't be blank");  
+        return false;  
+    }else if(!filter.test(name)|| password.length<8){  
+        alert("Incorrect Credentials");  
+        return false;  
+    }  
+    else{
+        window.location.href = 'index.html'
+        return true
+    }   
+}
+
+// register
+const validReg = ()=>{
+    var form = document.getElementById('form')
+    var name = document.getElementById('inputEmail0').value
+    var pass = document.getElementById('inputPassword').value
+    var pass1 = document.getElementById('inputPassword1').value
+    var mob = document.getElementById('inputMobile').value
+    var error = document.getElementById('errorMsg')
+    
+    form.addEventListener('submit',(e)=>{
+        
+        let messages = []
+
+        if(!filter.test(name)){
+            messages.push('Invalid email address')
+        }
+
+        if(pass.length<8 && pass==pass1){
+            messages.push('password length must be greater than 8 characters')
+        }
+        if(pass!=pass1){
+            messages.push('passwords did not matched')
+        }
+        if(mob.length!=10){
+            messages.push('please enter valid mobile number')
+        }
+        if(!gridCheck.checked){
+            messages.push("please agree to the T & C")
+        }
+        
+        if(messages.length>0){
+
+            e.preventDefault()
+            error.innerText = messages.join(', ')
+        }
+        else{
+            window.location.href  = 'SignIn.html'
+            
+            alert('Successfully Registered !!! Now Please login')
+            return true;
+            
+        }
+    })
+ }
+
+// for forgot password
+const checkEmail = ()=>{
+    var email = document.getElementById('email')
+    // var email = document.getElementById('txtEmail');
+    
+
+    if (!filter.test(email.value)) {
+    alert('Please provide a valid email address');
+    email.focus;
+    return false;
+    }
+    else{
+        alert('Reset Link sent to your registered email address')
+    }
+}
---
+++ js/toCart.js
@@ -0,0 +1,24 @@
+var cartItem = {
+    "chairs":[
+        {"Name":"Fluffy Chair", "Price":"5999"},
+        {"firstName":"Anna", "lastName":"Smith"},
+        {"firstName":"Peter", "lastName":"Jones"}
+    ]
+}
+
+var rate = [5999,12999,9999]
+// console.log(typeof(rate[0]))
+var qty1 = parseInt(document.getElementById('one').value);
+var qty2 = parseInt(document.getElementById('two').value);
+var qty3 = parseInt(document.getElementById('three').value);
+// console.log(qty1.value)
+var total;
+
+function giveTotal(){
+    console.log(typeof(rate[0]))
+
+   total = parseInt(qty1)*rate[0] + parseInt(qty2)*rate[1] + parseInt(qty3)*rate[3];
+   console.log(total)
+   document.getElementById('total-amt').innerText = parseInt(total)
+}
+
